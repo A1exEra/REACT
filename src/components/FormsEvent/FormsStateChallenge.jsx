a03ddfd454706bd3b65f19e31515b97bf34a3d7d
A1exEra <eramisant@yahoo.com>
2023-01-16 20:36:32 +0000

    add spread operator to forms state

--- src/components/FormsEvent/FormsStateChallenge.jsx
+++ src/components/FormsEvent/FormsStateChallenge.jsx
@@ -1,10 +1,48 @@
 import React, { useState } from "react";
 
 function FormStateChallenge() {
+  //get the ract state
+  const [contacts, setContacts] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+  });
+  //create a method to change the inputs and state ofthe react app
+  function handleChange(event) {
+    // get the holdof the event
+    const { name, value } = event.target;
+    // console.log(name);
+    // console.log(value);
+    //use react state to to chenge the state and display input values dynamically
+    // setContacts(() => {
+    //   if (name === "fName") {
+    //     return {
+    //       fName: value,
+    //       lName: contacts.lName,
+    //       email: contacts.email,
+    //     };
+    //   } else if (name === "lName") {
+    //     return {
+    //       fName: contacts.fName,
+    //       lName: value,
+    //       email: contacts.email,
+    //     };
+    //   } else {
+    //     return {
+    //       fName: contacts.fName,
+    //       lName: contacts.lName,
+    //       email: value,
+    //     };
+    //   }
+    // });
+    setContacts((prevValue) => ({ ...prevValue, [name]: value }));
+  }
   return (
     <div className="formContainer">
-      <h1>Hello, FirstName LastName!</h1>
-      <p>email</p>
+      <h1>
+        Hello, {contacts.fName} {contacts.lName}!
+      </h1>
+      <p>{contacts.email}</p>
       <form>
         <input
           type="text"
@@ -12,6 +50,9 @@ function FormStateChallenge() {
           id="firstName"
           placeholder="Name"
           style={{ width: "300px" }}
+          name="fName"
+          value={contacts.fName}
+          onChange={handleChange}
         />
         <input
           type="text"
@@ -19,6 +60,9 @@ function FormStateChallenge() {
           id="lastName"
           placeholder="Surname"
           style={{ width: "300px" }}
+          name="lName"
+          value={contacts.lName}
+          onChange={handleChange}
         />
         <input
           type="email"
@@ -26,6 +70,9 @@ function FormStateChallenge() {
           id="lastName"
           placeholder="Email"
           style={{ width: "300px" }}
+          name="email"
+          value={contacts.email}
+          onChange={handleChange}
         />
         <button type="button" class="btn btn-primary mx-auto">
           Submit
